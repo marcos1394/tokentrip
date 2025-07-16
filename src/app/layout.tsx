bf88fd432c5fc825107ca4c7b17411d9be6f3eca
Marcos Sandoval <76020778+marcos1394@users.noreply.github.com>
2025-07-15 19:08:29 -0700

    Update layout.tsx
    se envuelve con todos los proveedores

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,37 @@
 // src/app/layout.tsx
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import DappKitProviders from "@/components/DappKitProviders"; // Importamos el proveedor
+import { ThemeProvider } from "@/components/theme-provider";
 
-// Este es el layout más básico posible para satisfacer a Next.js.
-// No necesita tener estilos ni componentes complejos.
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "TokenTrip",
+  description: "The Decentralized Experience Economy",
+};
+
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        {/* Envolvemos todo con los proveedores aquí */}
+        <DappKitProviders>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </DappKitProviders>
+      </body>
     </html>
   );
 }
