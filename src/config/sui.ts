df51362d89cb231880190c783640158ac6821bd5
Marcos Sandoval <marcosbancaprepa@gmail.com>
2025-08-10 05:45:41 +0000

    prueba de display con getters y setters

--- src/config/sui.ts
+++ src/config/sui.ts
@@ -3,10 +3,10 @@
 // Configuración final actualizada para Testnet
 export const suiConfig = {
   // --- IDs de los PAQUETES (Actualizados de tus logs de publicación) ---
-  packageId: "0xcea6a3334cb989a2a7462c1fc22a43f87484bde7e51c6acf4a7e72ac1da04368", // tokentrip_experiences
-  auctionsPackageId: "0xa2de509885620271f1bee4e39582f5edb9bd5c446a7107e8da782b2e08fb34b9",
-  lendingPackageId: "0xe15e4b8e898f16034106345409280a30408c6b71c776e71345f6635390defde9",
-  rentalPackageId: "0xb49d58b4b84f995e14ee95ac549ac9dc8dc6dca9ebee6ab5fd02c9587f13224a",
+  packageId: "0xea217e700d2aa262cd91b7cbcccb899b976a1f2d7b63d700a860a7cdd4003286", // tokentrip_experiences
+  auctionsPackageId: "0xb0b85843bc219692ee6c35cdf43606568cdb23fe66a78ab9d6b753d8ec59c47c",
+  lendingPackageId: "0x1806798da34dc3a1624cd319ae37668bc684580b43d5d13a0b064b29b8ef43dc",
+  rentalPackageId: "0x78394a41097c40053bcbf56f77804685469813e0274e7369e2d593d982885f8f",
   
   // --- IDs de paquetes que NO cambiaron (dependencias) ---
   tktPackageId: "0xed46584d450fc173a7796cdbc934248df25f0d46700a3a852fb025f1d9a000b3",
@@ -15,9 +15,9 @@ export const suiConfig = {
   
   // --- IDs de OBJETOS creados por la función init ---
 
-  // Creados por `tokentrip_experiences` (packageId)
-  adminCapId: "0x9130b9cdbf04339db4805557ba594b39472b853724a63b05c7ec1b508cc63387",
-  vipRegistryId: "0x699c39c90a853e536260b16662074df63b18e69654583b0c59ad3345b5430728",
+  // Creados por la última versión de `tokentrip_experiences` (packageId)
+  adminCapId: "0xf6aa20625038bb7386b6d04de15e08200d5760b5d80ff051a4d0bc36575c2696",
+  vipRegistryId: "0x89437b91ff4abfdec5bacc790e405df02d36d650be40ddf7b3a6d01c43599b73",
 
   // IDs de objetos de otros paquetes (asumiendo que no se han vuelto a publicar)
   stakingPoolId: "0x1f10f55a350e29250a99bf76cf7d448df10474c190e87c9e1a974b58ebc06b67",
