c8f6f676651cb10fbc1556c942bba933870216a9
Marcos Sandoval <76020778+marcos1394@users.noreply.github.com>
2025-08-14 15:00:59 -0700

    Update next.config.mjs
    se agrega el aggregator de walrus como acceso remoto valido

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,16 @@ const nextConfig = {
 experimental: {
     scrollRestoration: true,
   },
+images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'aggregator.testnet.walrus.atalma.io',
+      },
+      // Puedes añadir otros aggregators aquí si quieres
+    ],
+  },
+    
 };
 
 export default nextConfig;
